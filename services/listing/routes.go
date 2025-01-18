ebc5629208bcea8e788227ac916634bd29b11c27
Shahin Salehi <shahin.salehi2@gmail.com>
2025-01-18 12:59:52 +0100

    makefile

--- services/listing/routes.go
+++ services/listing/routes.go
@@ -1,6 +1,9 @@
 package listing
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+)
 
 type Handler struct {
 }
@@ -16,6 +19,7 @@ func (h *Handler) RegisterRoutes(router *http.ServeMux) {
 // post
 func (h *Handler) InsertListing(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
+		slog.Info("Method not allowed", slog.Any("status", 405))
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
