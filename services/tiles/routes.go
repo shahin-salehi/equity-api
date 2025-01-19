7650961d0363bf424d587d75e5b63fe9631b9f08
Shahin Salehi <shahin.salehi2@gmail.com>
2025-01-19 12:48:05 +0100

    Insert tiles

--- services/tiles/routes.go
+++ services/tiles/routes.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/shahin-salehi/equity-api/db"
+	"github.com/shahin-salehi/equity-api/types"
 	"github.com/shahin-salehi/equity-api/utils"
 )
 
@@ -19,6 +20,42 @@ func NewHandler(repo db.CRUD) *Handler {
 
 func (h *Handler) RegisterRoutes(router *http.ServeMux) {
 	router.HandleFunc("/tiles", h.Readtiles)
+	router.HandleFunc("/tiles/insert", h.InsertTiles)
+}
+
+func (h *Handler) InsertTiles(w http.ResponseWriter, r *http.Request) {
+	// Reject wrong method
+	if r.Method != "POST" {
+		slog.Info("Method not allowed", slog.Any("status", 405))
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// read body
+	defer r.Body.Close()
+	var b []types.GeoTile
+	err := utils.DeserializeJSON(r, &b)
+	if err != nil {
+		slog.Error("failed to deserialize", slog.Any("error", err))
+		utils.WriteError(w, http.StatusUnprocessableEntity, nil)
+		return
+	}
+
+	// insert
+	err = h.db.InsertManyTiles(b)
+	if err != nil {
+		slog.Error("db returned error when called from handler", slog.Any("function", "InsertTiles"))
+		utils.WriteError(w, http.StatusInternalServerError, nil)
+		return
+	}
+
+	//ok
+	err = utils.SerializeJSON(w, http.StatusCreated, nil)
+	if err != nil {
+		slog.Error("Failed to write response, this shouldn't happen.", slog.Any("function", "InsertTiles"))
+		utils.WriteError(w, http.StatusInternalServerError, nil)
+		return
+	}
+
 }
 
 func (h *Handler) Readtiles(w http.ResponseWriter, r *http.Request) {
